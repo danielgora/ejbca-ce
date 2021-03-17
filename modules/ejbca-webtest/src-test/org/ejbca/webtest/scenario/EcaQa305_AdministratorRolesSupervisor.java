b05c0b2d413897b48743823a3a251fca13f49b28
johannaa <johanna.axner@primekey.com>
2021-03-17 10:24:49 +0100

    Updated to ECAQA305 and removed test cases not relevant.

--- modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa305_AdministratorRolesSupervisor.java
+++ modules/ejbca-webtest/src-test/org/ejbca/webtest/scenario/EcaQa305_AdministratorRolesSupervisor.java
@@ -9,15 +9,12 @@ import org.openqa.selenium.WebDriver;
 import java.util.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class EcaQa282_AdministratorRolesSupervisor extends WebTestBase {
+public class EcaQa305_AdministratorRolesSupervisor extends WebTestBase {
 
     //Classes
     private static CertificateProfileHelper certificateProfileHelper;
     private static EndEntityProfileHelper endEntityProfileHelper;
-    private static RaWebHelper raWebHelper;
     private static AdminRolesHelper adminRolesHelper;
-    private static SearchEndEntitiesHelper searchEndEntitiesHelper;
-    private static AddEndEntityHelper addEndEntityHelper;
 
     //Test Data
     private static class TestData {
@@ -30,17 +27,6 @@ public class EcaQa282_AdministratorRolesSupervisor extends WebTestBase {
         static final String END_ENTITY_PROFILE = "Supervisor";
         static final String ROLE_NAME = "Supervisor";
         static final String ROLE_TEMPLATE = "Supervisors";
-        static final String USER_NAME = "Supervisor";
-        static final String PASSWORD = "foo123";
-        static final String KEY_ALGORITHM = "RSA 4096 bits";
-        static final String COMMON_NAME = "Supervisor";
-        static final Map<String, String> END_ENTITY_FIELDMAP = new HashMap<>();
-        {
-            END_ENTITY_FIELDMAP.put("Username", TestData.USER_NAME);
-            END_ENTITY_FIELDMAP.put("Password (or Enrollment Code)", TestData.PASSWORD);
-            END_ENTITY_FIELDMAP.put("Confirm Password", TestData.PASSWORD);
-            END_ENTITY_FIELDMAP.put("CN, Common name", TestData.COMMON_NAME);
-        }
     }
 
     @BeforeClass
@@ -51,10 +37,7 @@ public class EcaQa282_AdministratorRolesSupervisor extends WebTestBase {
         // Init helpers
         certificateProfileHelper = new CertificateProfileHelper(webDriver);
         endEntityProfileHelper = new EndEntityProfileHelper(webDriver);
-        raWebHelper = new RaWebHelper(webDriver);
         adminRolesHelper = new AdminRolesHelper(webDriver);
-        searchEndEntitiesHelper = new SearchEndEntitiesHelper(webDriver);
-        addEndEntityHelper = new AddEndEntityHelper(webDriver);
     }
 
     @AfterClass
@@ -69,7 +52,6 @@ public class EcaQa282_AdministratorRolesSupervisor extends WebTestBase {
         removeCertificateProfileByName(TestData.CERTIFICATE_PROFILE_NAME_SUPERVISOR);
         removeEndEntityProfileByName(TestData.END_ENTITY_PROFILE);
         removeAdministratorRoleByName(TestData.ROLE_NAME);
-        removeEndEntityByUsername(TestData.USER_NAME);
     }
 
     @Test
@@ -101,8 +83,8 @@ public class EcaQa282_AdministratorRolesSupervisor extends WebTestBase {
     public void test3_AddEndEntityProfileAndAssertSaved() {
         //when
         endEntityProfileHelper.openPage(getAdminWebUrl());
-        endEntityProfileHelper.addEndEntityProfile(TestData.END_ENTITY_PROFILE);
         //then
+        endEntityProfileHelper.addEndEntityProfile(TestData.END_ENTITY_PROFILE);
         endEntityProfileHelper.assertEndEntityProfileNameExists(TestData.END_ENTITY_PROFILE);
     }
 
@@ -123,49 +105,30 @@ public class EcaQa282_AdministratorRolesSupervisor extends WebTestBase {
     }
 
     @Test
-    @Ignore
-    //Test incomplete.
-    public void test5_() throws InterruptedException {
+    public void test5_OpenAndAssertRolesManagementPageIsOpen() {
+        adminRolesHelper.openPage(getAdminWebUrl());
+    }
+
+    @Test
+    public void test6_AddRoleAndAssertExistsAndMessageDisplayed() {
         //when
-        certificateProfileHelper.openPage(getAdminWebUrl());
-        certificateProfileHelper.cloneCertificateProfile(TestData.CERTIFICATE_PROFILE_NAME_ENDUSER, TestData.CERTIFICATE_PROFILE_NAME_SUPERVISOR);
-        endEntityProfileHelper.openPage(getAdminWebUrl());
-        endEntityProfileHelper.addEndEntityProfile(TestData.END_ENTITY_PROFILE);
-        raWebHelper.openPage(getAdminWebUrl());
-        raWebHelper.makeNewCertificateRequest();
-        raWebHelper.selectCertificateTypeByEndEntityName(TestData.END_ENTITY_PROFILE);
-        raWebHelper.selectKeyPairGenerationOnServer();
-        raWebHelper.selectKeyAlgorithm(TestData.KEY_ALGORITHM);
-        raWebHelper.fillMakeRequestEditCommonName(TestData.COMMON_NAME);
-        raWebHelper.fillUsername(TestData.USER_NAME);
-        raWebHelper.fillEnrollUsernameAndCode(TestData.USER_NAME, TestData.PASSWORD);
-        raWebHelper.clickDownloadPkcs12();
+        adminRolesHelper.openPage(getAdminWebUrl());
         //then
-
-        //assert Supervisor.p12 is downloaded locally
-        //remove p12
-    }
-
-    @Test
-    public void test6_OpenAndAssertRolesManagementPageIsOpen() {
-        adminRolesHelper.openPage(getAdminWebUrl());
-    }
-
-    @Test
-    public void test7_AddRoleAndAssertExistsAndMessageDisplayed() {
-        adminRolesHelper.openPage(getAdminWebUrl());
         adminRolesHelper.addRole(TestData.ROLE_NAME);
     }
 
     @Test
-    public void test8_OpenEditAccessRulesAndAssertDisplayed() {
+    public void test7_OpenEditAccessRulesAndAssertDisplayed() {
+        //when
         adminRolesHelper.openPage(getAdminWebUrl());
         adminRolesHelper.addRole(TestData.ROLE_NAME);
+        //then
         adminRolesHelper.openEditAccessRulesPage(TestData.ROLE_NAME);
     }
 
     @Test
-    public void test9_AssertRoleTemplateSupervisorExists() {
+    public void test8_AssertRoleTemplateSupervisorExists() {
+        //when
         adminRolesHelper.openPage(getAdminWebUrl());
         adminRolesHelper.addRole(TestData.ROLE_NAME);
         adminRolesHelper.openEditAccessRulesPage(TestData.ROLE_NAME);
@@ -177,104 +140,23 @@ public class EcaQa282_AdministratorRolesSupervisor extends WebTestBase {
         adminRolesHelper.assertInternalKeybindingRulesIsEnabled(true);
         adminRolesHelper.assertOtherRulesHasAllSelected();
         adminRolesHelper.saveAccessRule();
+        //then
         adminRolesHelper.openPage(getAdminWebUrl());
         adminRolesHelper.openEditAccessRulesPage(TestData.ROLE_NAME);
         adminRolesHelper.assertRoleTemplateHasSelectedName(TestData.ROLE_TEMPLATE);
     }
 
     @Test
-    public void test10_UpdateCAAndEndEntityProfileInRoleAndAssertSaved() {
+    public void test9_UpdateCAAndEndEntityProfileInRoleAndAssertSaved() {
+        //when
         adminRolesHelper.openPage(getAdminWebUrl());
         adminRolesHelper.addRole(TestData.ROLE_NAME);
         adminRolesHelper.openEditAccessRulesPage(TestData.ROLE_NAME);
         adminRolesHelper.selectRoleTemplate("Supervisors");
         adminRolesHelper.selectAvailableSingleCa("All");
         adminRolesHelper.selectAvailableSingleEndEntityProfile("All");
+        //then
         adminRolesHelper.saveAccessRule();
-    }
-
-    @Test
-    @Ignore
-    // Test incomplete.
-    public void test11_SearchForEndEntityByUserNameAndAssertCertificateExists() {
-        //Add certificate
-        endEntityProfileHelper.openPage(getAdminWebUrl());
-        endEntityProfileHelper.addEndEntityProfile(TestData.END_ENTITY_PROFILE);
-        addEndEntityHelper.openPage(getAdminWebUrl());
-        addEndEntityHelper.setEndEntityProfile(TestData.END_ENTITY_PROFILE);
-        addEndEntityHelper.fillFields(TestData.END_ENTITY_FIELDMAP);
-        addEndEntityHelper.addEndEntity();
-        searchEndEntitiesHelper.openPage(getAdminWebUrl());
-        searchEndEntitiesHelper.fillSearchCriteria(TestData.USER_NAME, null, null, null);
-        searchEndEntitiesHelper.clickSearchByUsernameButton();
-        searchEndEntitiesHelper.assertNumberOfSearchResults(1);
-        searchEndEntitiesHelper.triggerSearchResultUsernameRowSelect(TestData.USER_NAME);
-        searchEndEntitiesHelper.clickViewCertificateForRow(TestData.USER_NAME);
-        //click on View button for certificate under actions for supervisor
-        //Note down 'certificate serial number'
-    }
-
-    @Test
-    @Ignore
-    //Test incomplete.
-    public void test12_() {
-        //Add certificate
-        adminRolesHelper.openPage(getAdminWebUrl());
-        adminRolesHelper.addRole(TestData.ROLE_NAME);
-        adminRolesHelper.openEditMembersPage(TestData.ROLE_NAME);
-        //input certificate serial number
-        // assert row content
-
-
-    }
-
-    @Test
-    @Ignore
-    //Test incomplete
-    public void test13_() {
-
-        //import supervisor.p12 to browser
-
-    }
-
-    @Test
-    @Ignore
-    //Test incomplete
-    public void test14_() {
-
-        //open incognito tab
-        //choose Supervisor's EJBCA Sample ID
-        //Assert message shown
-
-    }
-
-    @Test
-    @Ignore
-    //Test incomplete
-    public void test15_() {
-        // Open view Log
-        //is 'current conditions' and 'search result' visible
-
-    }
-
-    @Test
-    @Ignore
-    //Test incomplete
-    public void test16_() {
-        //Choose an 'Event' which has a 'Certificate Authority' available
-        //click on value for 'Certificate Authority'
-        //Assert 'CA Certificate' is displayed
-
-    }
-
-    @Test
-    @Ignore
-    //Test incomplete
-    public void test17_() {
-        //Open view Log
-        //Choose an 'Event with 'Certificate' available
-        //Click on value for 'Certificate'
-        //Assert 'View Certificate' page is displayed
     }
 
 }
