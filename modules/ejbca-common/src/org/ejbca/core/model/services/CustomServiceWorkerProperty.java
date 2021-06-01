93b0d6fd90b508532affb8d46b49bf25f9d70233
Mark Wright <mark.wright@primekey.com>
2021-06-01 15:42:35 -0400

    Add ability to selectively enable UI for services
    Add class IDs to CustomServiceWorkerProperty and javascript that can
    search for those that should be enabled based on the value of a
    selection box.

--- modules/ejbca-common/src/org/ejbca/core/model/services/CustomServiceWorkerProperty.java
+++ modules/ejbca-common/src/org/ejbca/core/model/services/CustomServiceWorkerProperty.java
@@ -12,7 +12,11 @@
  *************************************************************************/
 package org.ejbca.core.model.services;
 
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Helper class for rendering an UI for a custom service worker.
@@ -21,9 +25,9 @@ import java.util.List;
  */
 public class CustomServiceWorkerProperty {
 
-    public static final int UI_TEXTINPUT  = 0;
-    public static final int UI_SELECTONE  = 1;
-    public static final int UI_BOOLEAN    = 2;
+    public static final int UI_TEXTINPUT = 0;
+    public static final int UI_SELECTONE = 1;
+    public static final int UI_BOOLEAN = 2;
     public static final int UI_SELECTMANY = 3;
     public static final int UI_SECRETINPUT = 4;
 
@@ -32,7 +36,13 @@ public class CustomServiceWorkerProperty {
     private final List<String> options;
     private final List<String> optionTexts;
     private String value;
-    
+    private String enabledGroupId = UUID.randomUUID().toString();
+    private final HashMap<String, String> enabledWhenControlHasValue = new HashMap<>();
+
+    public void enableWhenControlHasValue(CustomServiceWorkerProperty controllingControl, String value) {
+        enabledWhenControlHasValue.put(controllingControl.enabledGroupId, Base64.getEncoder().encodeToString(value.getBytes()));
+    }
+
     /**
      * Construct a UI object with a list of options to choose from.
      * @param name The name of the property this object should map to.
@@ -41,7 +51,8 @@ public class CustomServiceWorkerProperty {
      * @param optionTexts Label of the options in the same order.
      * @param value The current value of this component.
      */
-    public CustomServiceWorkerProperty(final String name, final int type, final List<String> options, final List<String> optionTexts, final String value) {
+    public CustomServiceWorkerProperty(final String name, final int type, final List<String> options, final List<String> optionTexts,
+            final String value) {
         this.name = name;
         this.type = type;
         this.options = options;
@@ -67,32 +78,35 @@ public class CustomServiceWorkerProperty {
     public String getValue() {
         return value;
     }
-    
+
     /** Set the current value of this property (as String) */
     public void setValue(String value) {
-        if (value!=null) {
+        if (value != null) {
             value = value.trim();
         }
         this.value = value;
     }
-    
+
     /** @return the current value of this component as a boolean. */
     public boolean getBooleanValue() {
         return Boolean.valueOf(getValue());
     }
+
     /** Set the current value of this component as a boolean. */
     public void setBooleanValue(final boolean value) {
         setValue(Boolean.valueOf(value).toString());
     }
+
     /** @return the current value of this component as a "select many" component. */
     public String[] getMultiValue() {
         return getValue().split(";");
     }
+
     /** Set the current value of this component as a "select many" component. */
     public void setMultiValue(final String[] values) {
         final StringBuilder sb = new StringBuilder();
         for (final String value : values) {
-            if (sb.length()>0) {
+            if (sb.length() > 0) {
                 sb.append(';');
             }
             sb.append(value);
@@ -100,22 +114,61 @@ public class CustomServiceWorkerProperty {
         setValue(sb.toString());
     }
 
+    public String getCssClass() {
+        if (enabledWhenControlHasValue.isEmpty()) {
+            return "";
+        } else {
+            return enabledWhenControlHasValue.keySet().stream().map(c -> "enable-" + c + " enable-" + c + "-" + enabledWhenControlHasValue.get(c))
+                    .collect(Collectors.joining());
+        }
+    }
+
+    public String getEnabledGroupId() {
+        return enabledGroupId;
+    }
+
     /** @return the name of this property */
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
+
     /** @return one of the CustomPublisherProperty.UI_* constants */
-    public int getType() { return type; }
+    public int getType() {
+        return type;
+    }
+
     /** @return true if this is a free text input field. */
-    public boolean isTypeText() { return type == UI_TEXTINPUT; }
+    public boolean isTypeText() {
+        return type == UI_TEXTINPUT;
+    }
+
     /** @return true if this is a boolean input field. */
-    public boolean isTypeBoolean() { return type == UI_BOOLEAN; }
+    public boolean isTypeBoolean() {
+        return type == UI_BOOLEAN;
+    }
+
     /** @return true if this is a "select one" input field. */
-    public boolean isTypeSelectOne() { return type == UI_SELECTONE; }
+    public boolean isTypeSelectOne() {
+        return type == UI_SELECTONE;
+    }
+
     /** @return true if this is a "select many" input field. */
-    public boolean isTypeSelectMany() { return type == UI_SELECTMANY; }
+    public boolean isTypeSelectMany() {
+        return type == UI_SELECTMANY;
+    }
+
     /** @return true if this is a free text input field. */
-    public boolean isTypeSecret() { return type == UI_SECRETINPUT; }
+    public boolean isTypeSecret() {
+        return type == UI_SECRETINPUT;
+    }
+
     /** @return a List of values this property can have or null if this does not apply to the type */
-    public List<String> getOptions() { return options; }
+    public List<String> getOptions() {
+        return options;
+    }
+
     /** @return a List of user-friendly texts corresponding to the values this property can have or null if this does not apply to the type */
-    public List<String> getOptionTexts() { return optionTexts; }
+    public List<String> getOptionTexts() {
+        return optionTexts;
+    }
 }
