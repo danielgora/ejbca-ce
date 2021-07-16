09cf6476d3462953ede3c8ec8aa9fd11eaa53c8d
Bastian Fredriksson <bastian@primekey.com>
2021-07-16 11:58:36 +0200

    ECA-10193 Test to ensure correct EC curve is used.
    Signed-off-by: Bastian Fredriksson <bastian@primekey.com>

--- modules/cesecore-common/src-test/org/cesecore/certificates/ca/CAConstantsTest.java
+++ modules/cesecore-common/src-test/org/cesecore/certificates/ca/CAConstantsTest.java
@@ -13,18 +13,32 @@
 
 package org.cesecore.certificates.ca;
 
+import org.apache.log4j.Logger;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.cesecore.certificates.util.AlgorithmConstants;
 import org.cesecore.keys.util.KeyTools;
 import org.cesecore.util.CryptoProviderTools;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.security.PublicKey;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class CAConstantsTest {
+    private static final Logger log = Logger.getLogger(CAConstantsTest.class);
+
+    @Before
+    public void installBcProvider() {
+        CryptoProviderTools.installBCProviderIfNotAvailable();
+    }
 
     @Test
     public void testGetPreSignKeys() {
-        CryptoProviderTools.installBCProviderIfNotAvailable();
         assertNotNull("Should find presign DSA key",
                 CAConstants.getPreSignPublicKey(
                         AlgorithmConstants.SIGALG_SHA256_WITH_DSA,
@@ -39,7 +53,7 @@ public class CAConstantsTest {
                         KeyTools.getKeyPairFromPEM(CAConstants.PRESIGN_VALIDATION_KEY_EC_SECP256R1_PRIV).getPublic()));
         assertNotNull("Should find presign secp384r1 key",
                 CAConstants.getPreSignPublicKey(
-                        AlgorithmConstants.SIGALG_SHA256_WITH_ECDSA,
+                        AlgorithmConstants.SIGALG_SHA384_WITH_ECDSA,
                         KeyTools.getKeyPairFromPEM(CAConstants.PRESIGN_VALIDATION_KEY_EC_SECP384R1_PRIV).getPublic()));
         assertNotNull("Should find presign Ed25519 key",
                 CAConstants.getPreSignPublicKey(
@@ -49,5 +63,33 @@ public class CAConstantsTest {
                 CAConstants.getPreSignPublicKey(
                         AlgorithmConstants.KEYALGORITHM_ED448,
                         KeyTools.getKeyPairFromPEM(CAConstants.PRESIGN_VALIDATION_KEY_ED448_PRIV).getPublic()));
+    }
+
+    @Test
+    public void testP256Presign() {
+        final PublicKey preSignKey = CAConstants.getPreSignPublicKey(
+                AlgorithmConstants.SIGALG_SHA256_WITH_ECDSA,
+                KeyTools.getKeyPairFromPEM(CAConstants.PRESIGN_VALIDATION_KEY_EC_SECP256R1_PRIV).getPublic());
+        final byte[] encodedKey = preSignKey.getEncoded();
+        final SubjectPublicKeyInfo spki = SubjectPublicKeyInfo.getInstance(ASN1Sequence.getInstance(encodedKey));
+        final AlgorithmIdentifier algorithmIdentifier = spki.getAlgorithm();
+        final ASN1ObjectIdentifier oid = (ASN1ObjectIdentifier) algorithmIdentifier.getParameters();
+        log.info("OID: " + oid);
+        // http://oid-info.com/get/1.2.840.10045.3.1.7
+        assertEquals("presign key uses wrong curve (expected P-256)", "1.2.840.10045.3.1.7", oid.toString());
+    }
+
+    @Test
+    public void testP384Presign() {
+        final PublicKey preSignKey = CAConstants.getPreSignPublicKey(
+                AlgorithmConstants.SIGALG_SHA384_WITH_ECDSA,
+                KeyTools.getKeyPairFromPEM(CAConstants.PRESIGN_VALIDATION_KEY_EC_SECP384R1_PRIV).getPublic());
+        final byte[] encodedKey = preSignKey.getEncoded();
+        final SubjectPublicKeyInfo spki = SubjectPublicKeyInfo.getInstance(ASN1Sequence.getInstance(encodedKey));
+        final AlgorithmIdentifier algorithmIdentifier = spki.getAlgorithm();
+        final ASN1ObjectIdentifier oid = (ASN1ObjectIdentifier) algorithmIdentifier.getParameters();
+        log.info("OID: " + oid);
+        // http://oid-info.com/get/1.3.132.0.34
+        assertEquals("presign key uses wrong curve (expected P-384)", "1.3.132.0.34", oid.toString());
     }
 }
